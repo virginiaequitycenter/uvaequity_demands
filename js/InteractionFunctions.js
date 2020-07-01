caf88be52ee483a5ae50d2a7ae299c161d804eb6
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-07-01 14:56:55 -0400

    add the tooltips

--- js/InteractionFunctions.js
+++ js/InteractionFunctions.js
@@ -3,20 +3,22 @@ var mouseover = function(d) {
     d3.select("#tooltip")
       .style("visibility","visible");
     
-    d3.select(this)
+    var attribute =  d3.select(this);
+ 
+   attribute
       .style("border", "solid grey .0001px")
-      .style("opacity", .5)  
+      .style("opacity", .5); 
     
-    d3.select("#demandtext").text(d.Text + " " )
-
+    d3.select("#demandtext").text(d.Text + " " );
+    
 }
 
 
 var mousemove = function(d) {
     
     d3.select("#tooltip")
-      .style("left", d3.event.pageX -100  + "px")
-      .style("top", d3.event.pageY -110 + "px")
+     // .style("left", d3.event.pageX - 25 + "px")
+      //.style("top", d3.event.pageY -110 + "px")
 
   }
 
