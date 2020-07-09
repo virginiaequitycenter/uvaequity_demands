927ae4e99bad654743f979932dec3f493214fa87
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-07-09 00:13:08 -0400

    finally on github

--- Analysis/js/scrolldrivenanalysis.js
+++ Analysis/js/scrolldrivenanalysis.js
@@ -3,10 +3,11 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.to("#chordchart", {
     scrollTrigger:{
-       trigger: "#chordstory",
-        start: "top bottom",
+       trigger: "#chords",
+        start: "center center",
+        endTrigger: "#chordstory",
         end: "bottom top",
-        markers: true,
+//        markers: true,
         pin: "#chordchart",
         pinSpacing: false,
         onEnterBack: () => d3.select("#ThemeChange").selectAll(".form-control").remove(),
@@ -19,10 +20,11 @@ gsap.to("#chordchart", {
         
 gsap.to("#streamchart", {
     scrollTrigger:{
-       trigger: "#streamstory",
-        start: "top bottom",
+       trigger: "#streams",
+        start: "center center",
+        endTrigger: "#streamstory",
         end: "bottom top",
-        markers: true,
+//        markers: true,
         pin: "#streamchart",
         pinSpacing: false,
         onEnterBack: () => d3.select("#DocChange").selectAll(".form-control").remove(),
