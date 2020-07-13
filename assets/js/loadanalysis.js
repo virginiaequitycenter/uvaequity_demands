b5e1e0418bf6e292f65acc080e6d8ffe9ab541fd
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-07-13 16:31:25 -0400

    homepage thoughts

--- assets/js/loadanalysis.js
+++ assets/js/loadanalysis.js
@@ -4,26 +4,18 @@ var demos;
 
 function loadData() {
 
-    d3.csv("data/demands.csv").then(function (d) {
+    d3.csv("assets/data/demands.csv").then(function (d) {
         demands = d;
         streamgraph(demands);
         DrawStreamsControl(demands);
         
         
-        d3.csv("data/demos.csv").then(function(d){
+        d3.csv("assets/data/demos.csv").then(function(d){
         demos = d;
         drawdemos(demos);
-            initiatemovement();
+        initiatemovement();
     })
         
     });
-
-//    d3.csv("data/chord.csv").then(function (d) {
-//        chords = d;
-//        drawchords(chords, "Total");
-//    });
-    
-    
-
 
 }
