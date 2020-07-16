160288b7061d54cbdd484aae1d3a3157cba4e355
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-07-15 21:19:47 -0400

    finally fix the dropdowns

--- assets/js/timeline/InteractionFunctions.js
+++ assets/js/timeline/InteractionFunctions.js
@@ -32,9 +32,74 @@ var mouseleave = function (d) {
         .style("opacity", 1);
 }
 
+// This Draws the Color Filter Selector
+function DrawTagFilter(tags) {
 
+    var TagList = d3.map(tags, function (d) {
+        return d.Code;
+    }).keys();
+
+    var tagselector = d3.select(".tagfilter")
+        .append("select")
+        .classed("form-control", true)
+        .classed("tagselector", true)
+        .on("change", function () {
+            ColorDemands(tags, this.value);
+            console.log(this.value);
+        });
+
+    tagselector.selectAll("option")
+        .data(TagList)
+        .enter()
+        .append("option")
+        .attr("value", (d) => d)
+        .text((d) => d)
+        .classed("tagoption", true);
+}
+
+// This Draws the Year Jump thing
+function DrawYearJump(Data) {
+
+    var YearList = d3.map(Data, function (d) {
+        return d.YEAR;
+    }).keys();
+    
+    var addon = [""];
+    var YearList = addon.concat(YearList);
+
+    var yearjump = d3.select(".yearjump")
+        .append("select")
+        .classed("form-control", true)
+        .classed("yearjumpselect", true)
+    
+    yearjump.selectAll("option")
+        .data(YearList)
+        .enter()
+        .append("option")
+        .attr("value", (d) => "Y" + d)
+        .text((d) => d)
+        .classed("tagoption", true);
+
+
+    $('.yearjumpselect').change(function () {
+    
+        var data =  $(this).val();
+        var position =   $('#' + data).position().left;        
+     
+    $(".project").animate({scrollLeft: "+=" + position  }, 1000)
+      //  elmnt.scrollLeft += position
+        
+        var classList = document.getElementById(data).className.split(/\s+/);
+        // Update Doc ID for the year jumper
+        docid =  Number(classList[[0]]);
+        console.log(position, Number(classList[[0]]));
+        
+    });
+}
+
+
+// Year Jump Forward and Jump Back
 var docid = 1;
-
 
 $('#scroll-forward').click(function () {
     
@@ -65,31 +130,32 @@ $('#scroll-back').click(function () {
 
 });
 
+// Trying to make the enlarge unenlarge text on the timeline more maneagable
+var eventtexts = d3.selectAll(".eventtext");
+eventtexts.on("mouseover", position_tooltip) // On hover, launch the function below
 
-var ktooltips = document.querySelectorAll(".ktooltip");
-ktooltips.forEach(function(ktooltip, index){                // For each ktooltip
-  ktooltip.addEventListener("mouseover", position_tooltip); // On hover, launch the function below
-})
-
-function position_tooltip(){
-  // Get .ktooltiptext sibling
-  var tooltip = this.parentNode.querySelector(".ktooltiptext");
+ var position_tooltip = function (){
+// Get .ktooltiptext sibling
+//  var tooltip = this;
   
   // Get calculated ktooltip coordinates and size
-  var ktooltip_rect = this.getBoundingClientRect();
-
-  var tipX = ktooltip_rect.width + 5; // 5px on the right of the ktooltip
+  var eventtext_rect = $(this).getBoundingClientRect();
+     
+ console.log(eventtext_rect);
+    
+  var tipX = eventtext_rect.width + 5; // 5px on the right of the ktooltip
   var tipY = -40;                     // 40px on the top of the ktooltip
+  console.log(tipX)
   // Position tooltip
   tooltip.style.top = tipY + 'px';
   tooltip.style.left = tipX + 'px';
 
   // Get calculated tooltip coordinates and size
-  var tooltip_rect = tooltip.getBoundingClientRect();
+  var eventtext_rect = tooltip.getBoundingClientRect();
   // Corrections if out of window
-  if ((tooltip_rect.x + tooltip_rect.width) > window.innerWidth) // Out on the right
+  if ((eventtext_rect.x + eventtext_rect.width) > window.innerWidth) // Out on the right
     tipX = -tooltip_rect.width - 5;  // Simulate a "right: tipX" position
-  if (tooltip_rect.y < 0)            // Out on the top
+  if (eventtext_rect.y < 0)            // Out on the top
     tipY = tipY - tooltip_rect.y;    // Align on the top
 
   // Apply corrected position
