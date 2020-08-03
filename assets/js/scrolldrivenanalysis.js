9b04e3c3a2c64852cb208f842f0d59d2e5282ef5
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-08-03 17:03:08 -0400

    sidebar now works

--- assets/js/scrolldrivenanalysis.js
+++ assets/js/scrolldrivenanalysis.js
@@ -3,170 +3,170 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initiatemovement() {
 
-//var chartoffset = document.getElementById('streams').getBoundingClientRect().height/2
-//// Animate Stream Chart
-//
-//var streammargin = "calc(50vh' + chartoffset')'";
+    //var chartoffset = document.getElementById('streams').getBoundingClientRect().height/2
+    //// Animate Stream Chart
+    //
+    //var streammargin = "calc(50vh' + chartoffset')'";
 
-//d3.select("#streamstory").style("margin-bottom", streammargin);
-    
-gsap.to("#streamchart", {
-    scrollTrigger: {
-        trigger: "#streamchart",
-        start: "center center",
-        endTrigger: "#streamstory",
-        end: "bottom top",
-//                markers: true,
-        pin: "#streamchart",
-        pinSpacing: false,
-        onEnterBack: function () {
-    updatestreams(demands, "Muddy Floor Report")
-         thestreams
-                .on("mouseover", function () {})
-                .on("mousemove", function () {})
-                .on("mouseleave", function () {});
+    //d3.select("#streamstory").style("margin-bottom", streammargin);
+
+    gsap.to("#streamchart", {
+        scrollTrigger: {
+            trigger: "#streamchart",
+            start: "center center",
+            endTrigger: "#streamstory",
+            end: "bottom top",
+            //                markers: true,
+            pin: "#streamchart",
+            pinSpacing: false,
+            onEnterBack: function () {
+                updatestreams(demands, "Muddy Floor Report")
+                thestreams
+                    .on("mouseover", function () {})
+                    .on("mousemove", function () {})
+                    .on("mouseleave", function () {});
+            },
+            onLeave: function () {
+                updatestreams(demands, "Student Body Referendum")
+
+            }
         },
-        onLeave: function () {
-            updatestreams(demands, "Student Body Referendum")
 
-        }
-    },
+    });
 
-});
-
-gsap.to("#streamchart", {
-    scrollTrigger: {
-        trigger: "#text1",
-        start: "center center",
-        //      endTrigger: "#streamstory",
-        end: "top top",
-        //        markers: true,
-        scrub: 1
-    },
-    opacity: 1
-});
-
-gsap.to("#text1", {
-    scrollTrigger: {
-        trigger: "#text1",
-        start: "bottom bottom",
-        //      endTrigger: "#streamstory",
-        end: +10,
-        //        markers: true,
-        scrub: 1
-    },
-    opacity: 1
-});
-
-var myAreas = d3.selectAll(".myArea");
-
-gsap.to(myAreas, {
-    scrollTrigger: {
-        trigger: "#text3",
-        start: "center center",
-        //      endTrigger: "#streamstory",
-        end: +10,
-        //        markers: true,
-        //        scrub: 1,
-
-        onEnterBack: function () {
-            thestreams.style("opacity", function (d) {
-                return opacitystreams(d.key);
-            })
+    gsap.to("#streamchart", {
+        scrollTrigger: {
+            trigger: "#text1",
+            start: "center center",
+            //      endTrigger: "#streamstory",
+            end: "top top",
+            //        markers: true,
+            scrub: 1
         },
-        onLeave: function () {
-            thestreams.style("opacity", .1)
-            d3.select("#MinorityStudentRecruitment").style("opacity", .9)
+        opacity: 1
+    });
+
+    gsap.to("#text1", {
+        scrollTrigger: {
+            trigger: "#text1",
+            start: "bottom bottom",
+            //      endTrigger: "#streamstory",
+            end: +10,
+            //        markers: true,
+            scrub: 1
         },
-    },
-});
+        opacity: 1
+    });
 
+    var myAreas = d3.selectAll(".myArea");
 
+    gsap.to(myAreas, {
+        scrollTrigger: {
+            trigger: "#text3",
+            start: "center center",
+            //      endTrigger: "#streamstory",
+            end: +10,
+            //        markers: true,
+            //        scrub: 1,
 
-gsap.to(myAreas, {
-    scrollTrigger: {
-        trigger: "#text4",
-        start: "center center",
-        //      endTrigger: "#streamstory",
-        end:+10,
-        //        markers: true,
-        //        scrub: 1,
-
-        onEnterBack: function () {
-            thestreams.style("opacity", .1)
-            d3.select("#MinorityStudentRecruitment").style("opacity", .9)
+            onEnterBack: function () {
+                thestreams.style("opacity", function (d) {
+                    return opacitystreams(d.key);
+                })
+            },
+            onLeave: function () {
+                thestreams.style("opacity", .1)
+                d3.select("#MinorityStudentRecruitment").style("opacity", .9)
+            },
         },
-        onLeave: function () {
-            thestreams.style("opacity", .1)
-           d3.select("#AbolitionDefunding").style("opacity", .9)
+    });
+
+
+
+    gsap.to(myAreas, {
+        scrollTrigger: {
+            trigger: "#text4",
+            start: "center center",
+            //      endTrigger: "#streamstory",
+            end: +10,
+            //        markers: true,
+            //        scrub: 1,
+
+            onEnterBack: function () {
+                thestreams.style("opacity", .1)
+                d3.select("#MinorityStudentRecruitment").style("opacity", .9)
+            },
+            onLeave: function () {
+                thestreams.style("opacity", .1)
+                d3.select("#AbolitionDefunding").style("opacity", .9)
+            },
         },
-    },
-});
+    });
 
 
-gsap.to(myAreas, {
-    scrollTrigger: {
-        trigger: "#text5",
-        start: "center center",
-        //      endTrigger: "#streamstory",
-        end:+10,
-        //        markers: true,
-        //        scrub: 1,
+    gsap.to(myAreas, {
+        scrollTrigger: {
+            trigger: "#text5",
+            start: "center center",
+            //      endTrigger: "#streamstory",
+            end: +10,
+            //        markers: true,
+            //        scrub: 1,
 
-        onEnterBack: function () {
-        updatestreams(demands, "Student Body Referendum")
+            onEnterBack: function () {
+                updatestreams(demands, "Student Body Referendum")
 
-        thestreams.style("opacity", .1);
-         d3.selectAll("#AbolitionDefunding").style("opacity", .9);  
-            
-        thestreams.on("mouseover", function () {})
-                .on("mousemove", function () {})
-                .on("mouseleave", function () {});
-       
-            
+                thestreams.style("opacity", .1);
+                d3.selectAll("#AbolitionDefunding").style("opacity", .9);
+
+                thestreams.on("mouseover", function () {})
+                    .on("mousemove", function () {})
+                    .on("mouseleave", function () {});
+
+
+            },
+            onLeave: function () {
+                updatestreams(demands, "An Audacious Faith")
+                thestreams
+                    .on("mouseover", function () {})
+                    .on("mousemove", function () {})
+                    .on("mouseleave", function () {})
+
+            },
         },
-        onLeave: function () {
-            updatestreams(demands, "An Audacious Faith")
-            thestreams
-                .on("mouseover", function () {})
-                .on("mousemove", function () {})
-                .on("mouseleave", function () {}) 
-            
-        },
-    },
-});
+    });
 
 
-gsap.to(myAreas, {
-    scrollTrigger: {
-        trigger: "#text6",
-        start: "center center",
-        //      endTrigger: "#streamstory",
-        end:+10,
-        //        markers: true,
-        //        scrub: 1,
+    gsap.to(myAreas, {
+        scrollTrigger: {
+            trigger: "#text6",
+            start: "center center",
+            //      endTrigger: "#streamstory",
+            end: +10,
+            //        markers: true,
+            //        scrub: 1,
 
-        onEnterBack: function () {
-            updatestreams(demands, "An Audacious Faith")
-            thestreams
-                .on("mouseover", function () {})
-                .on("mousemove", function () {})
-                .on("mouseleave", function () {}) 
-       
-            
+            onEnterBack: function () {
+                updatestreams(demands, "An Audacious Faith")
+                thestreams
+                    .on("mouseover", function () {})
+                    .on("mousemove", function () {})
+                    .on("mouseleave", function () {})
+
+
+            },
+            onLeave: function () {
+
+                updatestreams(demands, "Muddy Floor Report")
+                thestreams
+                    .on("mouseover", function () {})
+                    .on("mousemove", function () {})
+                    .on("mouseleave", function () {})
+
+            },
         },
-        onLeave: function () {
-            
-         updatestreams(demands, "Muddy Floor Report")
-         thestreams
-                .on("mouseover", function () {})
-                .on("mousemove", function () {})
-                .on("mouseleave", function () {}) 
- 
-        },
-    },
-});
-    
+    });
+
     gsap.to("#iframesearch", {
         scrollTrigger: {
             trigger: "#search",
@@ -180,19 +180,98 @@ gsap.to(myAreas, {
                 var ifr = document.getElementsByName('keysearch')[0]
                 ifr.src = "https://commpaslab.shinyapps.io/demandkeywords/"
             },
-//            onLeaveBack: function(){
-//                var ifr = document.getElementsByName('keysearch')[0]
-//                ifr.src = "#"
-//            }
+            //            onLeaveBack: function(){
+            //                var ifr = document.getElementsByName('keysearch')[0]
+            //                ifr.src = "#"
+            //            }
 
         },
+    });
 
+
+
+
+    gsap.to("#tocify-header1", {
+        scrollTrigger: {
+            trigger: "#mainNav",
+            start: "top top",
+            endTrigger: "#when",
+            end: "top top",
+            toggleClass: {
+                targets: "#tocify-header1",
+                className: "tocify-highlighted"
+            }
+        },
+    });
+
+
+    gsap.to("#tocify-header2", {
+        scrollTrigger: {
+            trigger: "#when",
+            start: "top top",
+            endTrigger: "#who",
+            end: "top top",
+            toggleClass: {
+                targets: "#tocify-header2",
+                className: "tocify-highlighted"
+            }
+        },
+    });
+
+    
+    
+    gsap.to("#tocify-header3", {
+        scrollTrigger: {
+            trigger: "#who",
+            start: "top top",
+            endTrigger: "#what",
+            end: "top top",
+            toggleClass: {
+                targets: "#tocify-header3",
+                className: "tocify-highlighted"
+            }
+        },
+    });
+    
+    gsap.to("#tocify-header4", {
+        scrollTrigger: {
+            trigger: "#what",
+            start: "top top",
+            endTrigger: "#search",
+            end: "top top",
+            toggleClass: {
+                targets: "#tocify-header4",
+                className: "tocify-highlighted"
+            }
+        },
     });
     
     
-   
+    gsap.to("#tocify-header5", {
+        scrollTrigger: {
+            trigger: "#search",
+            start: "top top",
+            endTrigger: "#footer",
+            end: "top top",
+            toggleClass: {
+                targets: "#tocify-header5",
+                className: "tocify-highlighted"
+            }
+        },
+    });
     
-    
+    gsap.to("#tocify-header5", {
+        scrollTrigger: {
+            trigger: "#iframesearch",
+            start: "top center",
+            endTrigger: "#footer",
+            end: "top top",
+            toggleClass: {
+                targets: "#TOC",
+                className: "tocify-stick"
+            }
+        },
+    });
+
+
 };
-
-
