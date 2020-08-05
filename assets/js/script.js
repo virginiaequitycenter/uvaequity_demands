7467c8b5a1cc32cbc1ea8b8ca67c0741ce6af23a
Sam Powers <35611153+sam-powers@users.noreply.github.com>
2020-08-05 00:26:33 -0400

    some small edits for structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-
-// Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 50) {
-      $("#mainNav")
-//      .removeClass("fixed-top")
-          .addClass("bg-brown");
-    } else {
-      $("#mainNav")
-//          .addClass("fixed-top")
-          .removeClass("bg-brown");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
+//
+//// Activate scrollspy to add active class to navbar items on scroll
+//  $('body').scrollspy({
+//    target: '#mainNav',
+//    offset: 70
+//  });
+//
+//  // Collapse Navbar
+//  var navbarCollapse = function() {
+//    if ($("#mainNav").offset().top > 10) {
+//      $("#mainNav")
+////      .removeClass("fixed-top")
+//          .addClass("bg-brown");
+//    } else {
+//      $("#mainNav")
+////          .addClass("fixed-top")
+//          .removeClass("bg-brown");
+//    }
+//  };
+//  // Collapse now if page is not at top
+//  navbarCollapse();
+//  // Collapse the navbar when page is scrolled
+//  $(window).scroll(navbarCollapse);
+//
 
 
 d3.csv("assets/data/picture_grid.csv").then(function (d) {
@@ -57,7 +57,8 @@ function drawProjects(photos) {
 imageboxes.append("img")
         .attr("src", function (d) {
             return "assets/front-images/" + d.image + "" + d.type
-        }).style("z-index", (d) => d.z)
+        })
+//    .style("z-index", (d) => d.z)
 
 imageboxes.append("div").classed("overlay", true);
 
